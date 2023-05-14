75e018cc6b6bdeb614aaa1aa206262b15613c603
Javier Rogina Gallego <j.rogina.2016@alumnos.urjc.es>
2023-05-14 18:12:42 +0200

    Initial commit

---
+++ main_panels_ocr.py
@@ -0,0 +1,28 @@
+import argparse
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description='Trains and executes a given detector over a set of testing images')
+    parser.add_argument(
+        '--detector', type=str, nargs="?", default="", help='Detector string name')
+    parser.add_argument(
+        '--train_path', default="", help='Select the training data dir')
+    parser.add_argument(
+        '--test_path', default="", help='Select the testing data dir')
+
+    args = parser.parse_args()
+
+    # Load training data
+
+    # Create the OCR classifier
+
+    # Load testing data
+
+    # Evaluate OCR over road panels
+
+
+
+
+
